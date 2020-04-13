280c67ce824762c61af09978a9c5af5b8f1b6763
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-04-14 02:11:04 +0530

    deployment

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -15,8 +14,8 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
+      .then(response => response.json())
+      .then(users => { this.setState({ robots: users }) });
   }
 
   onSearchChange = (event) => {
@@ -25,7 +24,7 @@ class App extends Component {
 
   render() {
     const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(robot =>{
+    const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
     return !robots.length ?
@@ -33,10 +32,8 @@ class App extends Component {
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
         </div>
       );
   }
